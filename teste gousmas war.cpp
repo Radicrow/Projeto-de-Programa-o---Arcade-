d748e7f089429e3bd77c9e6eea021e591f120373
Edgar Klewert <127999109+ZEDSH1@users.noreply.github.com>
2023-03-16 07:58:06 -0300

    Add files via upload

---
+++ teste gousmas war.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	
+	int jogador_aleatorio = rand() 2 + 1;
+	int jogador1_gousma1_furia = 0;
+	int jogador1_gousma2_furia = 0;
+	int jogador2_gousma1_furia = 0;
+	int jogador2_gousma2_furia = 0;
+	int opcao
+	
+	strand(time(NULL));
+	
+	while:(jogador1_gousma1_furia >= 0 && jogador1_gousma2_furia >= 0 && jogador2_gousma1_furia >= 0 && jogador2_gousma2_furia >= 0){
+		if(jogador_aleatorio == 0);
+		
+    printf("jogador1 escolha uma opçao: \n");
+    printf("atacar \n");
+    printf("dividir \n");
+    scanf("%D 0", &opcao);
+    
+	
+	return 0;
+}
